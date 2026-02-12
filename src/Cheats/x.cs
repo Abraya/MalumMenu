8eb5a0395be0877cc107a4b2897f16313f2d1c6e
Abraya <96598298+Abraya@users.noreply.github.com>
2026-02-12 22:18:00 +0000

    Implement toggle functionality for camera settings

--- src/Cheats/x.cs
+++ src/Cheats/x.cs
@@ -1,1 +1,34 @@
 
+using UnityEngine;
+using Sentry.Internal.Extensions;
+
+namespace MalumMenu;
+public static class x
+{
+    public static bool z;
+    
+    public static void x(HudManager hudManager)
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (z)
+            {
+            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = false;
+            Camera.main.gameObject.GetComponent<FollowerCamera>().Target = null;
+            Camera.main.transform.position = new Vector3(20.5f, -13.0f, 0.0f);
+            Camera.main.orthographicSize = 13.8f;
+            hudManager.UICamera.orthographicSize = 13.8f;
+            Utils.adjustResolution();
+            z = false;
+            }else{
+            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = true;
+            Camera.main.gameObject.GetComponent<FollowerCamera>().SetTarget(PlayerControl.LocalPlayer);
+            Camera.main.orthographicSize = 3f;
+            hudManager.UICamera.orthographicSize = 3f;
+            Utils.adjustResolution();
+            z = true;
+            }
+        }
+    }
+}
+
