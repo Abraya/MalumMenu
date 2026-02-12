2139e1056dafd9d4a29b90735078acf6395dd2e5
Abraya <96598298+Abraya@users.noreply.github.com>
2026-02-12 23:29:10 +0000

    Invert z value logic in y method

--- src/Cheats/x.cs
+++ src/Cheats/x.cs
@@ -10,7 +10,7 @@ public static class x
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
-            if (z)
+            if (!z)
             {
             Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = false;
             Camera.main.gameObject.GetComponent<FollowerCamera>().Target = null;
@@ -18,14 +18,14 @@ public static class x
             Camera.main.orthographicSize = 13.8f;
             hudManager.UICamera.orthographicSize = 13.8f;
             Utils.adjustResolution();
-            z = false;
+            z = true;
             }else{
             Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = true;
             Camera.main.gameObject.GetComponent<FollowerCamera>().SetTarget(PlayerControl.LocalPlayer);
             Camera.main.orthographicSize = 3f;
             hudManager.UICamera.orthographicSize = 3f;
             Utils.adjustResolution();
-            z = true;
+            z = false;
             }
         }
     }
