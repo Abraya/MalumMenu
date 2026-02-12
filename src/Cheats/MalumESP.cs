42e2e5e4eb7ac0871c120e7f3a649ad1232dba73
Abraya <96598298+Abraya@users.noreply.github.com>
2026-02-12 22:24:55 +0000

    Update MalumESP.cs

--- src/Cheats/MalumESP.cs
+++ src/Cheats/MalumESP.cs
@@ -158,27 +158,4 @@ public static class MalumESP
             }
         }
     }
-    public static void x(HudManager hudManager)
-    {
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            if (z)
-            {
-            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = false;
-            Camera.main.gameObject.GetComponent<FollowerCamera>().Target = null;
-            Camera.main.transform.position = new Vector3(20.5f, -13.0f, 0.0f);
-            Camera.main.orthographicSize = 13.8f;
-            hudManager.UICamera.orthographicSize = 13.8f;
-            Utils.adjustResolution();
-            z = false;
-            }else{
-            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = true;
-            Camera.main.gameObject.GetComponent<FollowerCamera>().SetTarget(PlayerControl.LocalPlayer);
-            Camera.main.orthographicSize = 3f;
-            hudManager.UICamera.orthographicSize = 3f;
-            Utils.adjustResolution();
-            z = true;
-            }
-        }
-    }
 }
